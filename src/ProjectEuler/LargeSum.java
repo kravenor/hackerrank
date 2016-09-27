d75b1c959d9767c7217ce96dbe13540c179cec30
kravenor <baldelli.simone@gmail.com>
2016-09-27 12:21:17 +0200

    Create LargeSum.java
    Project Euler #13: Large sum

---
+++ src/ProjectEuler/LargeSum.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    int length = scan.nextInt(); 
+    scan.nextLine();
+    // using BigInteger : Initializing at 0
+    BigInteger sum = new BigInteger("0");
+    String temp;
+    for(int x=0;x<length;x++){
+        temp = scan.nextLine();
+        BigInteger tempInt = new BigInteger(temp);      
+        sum = sum.add(tempInt);
+    }
+    String result =  sum + "";
+   System.out.println(result.substring(0,10));
+    }
+}
