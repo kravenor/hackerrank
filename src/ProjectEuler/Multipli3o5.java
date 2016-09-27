05b481e333f22cf46712f41150bedde5bec16e22
Simone <Simone@Asus>
2016-09-27 12:18:53 +0200

    Project Euler #1: Multiples of 3 and 5

--- src/ProjectEuler/Multipli3o5.java
+++ src/ProjectEuler/Multipli3o5.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ProjectEuler;
 
 import java.math.BigInteger;
@@ -10,7 +5,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Simone
+ * @author Simone Baldelli
  */
 public class Multipli3o5 {
     public static void main(String[]args){
